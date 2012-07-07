62626415473ad9e67bef8642ff7a002af8a99796
Nick Daly <Nick.M.Daly@gmail.com>
2012-07-07 12:09:26 -0500

    Added test script.

---
+++ ugly_hacks/santiago/scripts/test_freedombuddyLocation.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+"""Tests for the FreedomBuddy Location reporter.
+
+Tests enforce the behaviors documented in ``freedombuddy-location.py``.
+
+Unfortunately, we can't test command-line argument errors.  ``parser.error``
+quits the interpreter, so testing stops whenever we test an error condition.
+
+"""
+
+import unittest
+import freedombuddyLocation
+
+pass
