8884452118a18e524b02be9cb65a2cffdee55039
Nick Daly <Nick.M.Daly@gmail.com>
2012-07-07 11:16:00 -0500

    Documented expected behavior.

--- ugly_hacks/santiago/scripts/freedombuddy-location.py
+++ ugly_hacks/santiago/scripts/freedombuddy-location.py
@@ -7,13 +7,54 @@ accepts a key that identifies a trusted party and the service to show locations
 for.  It can show where someone else is hosting a service for me and it can show
 where I am hosting a service for a client.  It will print one location per line.
 
-:TODO: handle each of the options
+:TODO: the below:
+
+If key or service isn't specified: quit.
+
+If client and host are specified: quit.
+
+If force-query and noquery and host are specified: quit.
+
+If client is specified: just pull the list of locations from the cache and quit.
+
+If noquery is specified: skip querying and just pull the list of locations from
+the cache and quit.
+
+Until I implement active-request polling and between-request timeouts:
+
+    query the host.
+
+    wait the timeout.
+
+    report the locations of the (now) locally known services and quit.
+
+After I implement active-request polling:
+
+    query the host.
+
+    poll the list of active requests until the active request is handled or the
+    timeout elapses.
+
+    report the locations of the (now) locally known services and quit.
+
+After I implement between-request timeouts:
+
+    if (force-query is specified) or (the timeout has elapsed and noquery isn't
+    specified): query the host.
+
+    poll the list of active requests until the active request is handled or the
+    timeout elapses.
+
+    report the list of the (now) locally known services and quit.
+
 """
 
 from optparse import OptionParser
 import sys
 
-if __name__ == "__main__":
+def handle_args(args):
+    """Convert command-line arguments into options."""
+
     parser = OptionParser()
     parser.add_option("-k", "--key", dest="key",
                       help="Find services for or by this buddy.")
@@ -54,4 +95,9 @@ are specified, query with normal respect for the timeout.
 TODO: Implement this option.
 """)
 
-    (options, args) = parser.parse_args(sys.argv[1:])
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+
+    (options, args) = handle_args(sys.argv[1:])
