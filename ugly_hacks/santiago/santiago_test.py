d5998b019aa86fdb307bd6313cbe872e0de94f13
Nick Daly <Nick.M.Daly@gmail.com>
2012-07-14 12:19:40 -0500

    Completed default launch script:
    - Take useful commandline args (see --help).
    - Load settings from config files.
    - Automatically open browser to web-interface.

--- ugly_hacks/santiago/santiago_test.py
+++ ugly_hacks/santiago/santiago_test.py
@@ -1,65 +1,132 @@
-#! /usr/bin/env python # -*- mode: auto-fill; fill-column: 80 -*-
+#! /usr/bin/env python # -*- mode: python; mode: auto-fill; fill-column: 80; -*-
 
+"""Starts a test FreedomBuddy service.
+
+Good for testing that it'll actually run and start up.  By default, it'll start
+listening for connections on ``https://localhost:8080``.  It'll be hosting the
+FreedomBuddy service for itself and be able to learn and provide its own
+services to itself.  That will allow you to add additional services as
+necessary, that you can then provide to yourself or others.
+
+"""
+
+import ConfigParser as configparser
 import logging
+from optparse import OptionParser
 import sys
 import utilities
-import ConfigParser as configparser
+import webbrowser
+
 import santiago
 
-if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.DEBUG)
-    logging.getLogger("cherrypy.error").setLevel(logging.CRITICAL)
+def parse_args(args):
+    """Interpret args passed in on the command line."""
 
-    config = "production.cfg"
-    new_services = False
-    try:
-        config = sys.argv[1]
-        new_services = sys.argv[2]
-    except IndexError:
-        pass
+    parser = OptionParser()
+    parser.add_option("-v", "--verbose", dest="verbose", action="count",
+                      help="""\
+Can be given multiple times to increase logging level.  Once means show
+FreedomBuddy logging messages.  Twice means show protocol logging messages as
+well.""")
+    parser.add_option("-c", "--config", dest="config", default="production.cfg",
+                      help="""The configuration file to use.""")
 
-    # get my key, if possible
+    parser.add_option("-d", "--default-services", dest="default_services",
+                      action="store_true", help="""\
+Whether to reset the list of hosted and consumed services to the default.
+""")
+    parser.add_option("-t", "--trace", dest="trace", action="store_true",
+                      help="Drop into the debugger when starting FreedomBuddy.")
+
+    return parser.parse_args(args)
+
+def load_config(options):
+    """Load data from the specified configuration file."""
+
+    config = utilities.load_config(options.config)
+
+    mykey = safe_load(config, "pgpprocessor", "keyid", 0)
+    lang = safe_load(config, "general", "locale", "en")
+    protocols = [safe_load(config, "connectors", "protocols", {})]
+    connectors = {}
+
+    # loop through the protocols, finding connectors each protocol uses
+    # load the settings for each connector.
+    for protocol in protocols:
+        protocol_connectors = safe_load(config, protocol, "connectors",
+            [ protocol + "-listener", protocol + "-sender",
+              protocol + "-monitor" ])
+
+        # when we do load data from the file, it's a comma-delimited string
+        if hasattr(protocol_connectors, "split"):
+            protocol_connectors = protocol_connectors.split(", ")
+
+        for connector in protocol_connectors:
+            connectors[connector] = dict(safe_load(config, connector, None, {}))
+
+    return mykey, lang, protocols, connectors
+
+def safe_load(config, section, key=None, default=None):
+    """Safely load data from a configuration file."""
+
     try:
-        mykey = utilities.load_config(config).get("pgpprocessor",
-                                                  "keyid")
-        lang = utilities.load_config(config).get("general",
-                                                 "locale")
+        if key is not None:
+            return config.get(section, key)
+        else:
+            return config.items(section)
     except configparser.NoSectionError:
-        mykey = 0
-        lang = None
+        return default
 
-    # set up monitors, listeners, and senders
-    protocol = "https"
-    serving_port = 8080
-    cert = "santiago.crt"
 
-    listeners = { protocol: { "socket_port": serving_port,
-                             "ssl_certificate": cert,
-                             "ssl_private_key": cert
-                              }, }
-    senders = { protocol: { "proxy_host": "localhost",
-                           "proxy_port": 8118} }
-    monitors = { protocol: {} }
+if __name__ == "__main__":
+
+    (options, args) = parse_args(sys.argv)
+
+    if options.verbose > 0:
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.getLogger("cherrypy.error").setLevel(logging.CRITICAL)
+    if options.verbose > 1:
+        logging.getLogger("cherrypy.error").setLevel(logging.DEBUG)
+
+    # load configuration settings
+    (mykey, lang, protocols, connectors) = load_config(options)
+
+    listeners = {}
+    senders = {}
+    monitors = {}
+
+    # create listeners and senders
+    # FIXME figure out why it's incorrectly including keyid.
+    for protocol in protocols:
+        for connector in connectors:
+            if connector.endswith("listener"):
+                listeners[protocol] = dict(connectors[protocol + "-listener"])
+            elif connector.endswith("sender"):
+                senders[protocol] = dict(connectors[protocol + "-sender"])
+            elif connector.endswith("monitor"):
+                monitors[protocol] = dict(connectors[protocol + "-monitor"])
 
     # services to host and consume
-    service = "freedombuddy"
-    location = protocol + "://localhost"
+    url = "https://localhost:8080"
 
-    # go!
-    if new_services:
-        hosting = { mykey: { service: [location + ":" + str(serving_port)] } }
-        consuming = { mykey: { service: [location + ":" + str(serving_port)] } }
+    # configure system
+    if options.default_services:
+        service = "freedombuddy"
+        hosting = { mykey: { service: [url] } }
+        consuming = { mykey: { service: [url] } }
 
-        freedombuddy = santiago.Santiago(listeners, senders,
-                                     hosting, consuming,
-                                     me=mykey, monitors=monitors, locale=lang)
+        freedombuddy = santiago.Santiago(listeners, senders, hosting, consuming,
+                                         me=mykey, monitors=monitors,
+                                         locale=lang)
     else:
-        freedombuddy = santiago.Santiago(listeners, senders,
-                                     me=mykey, monitors=monitors, locale=lang)
-        
+        freedombuddy = santiago.Santiago(listeners, senders, me=mykey,
+                                         monitors=monitors, locale=lang)
 
-    # import pdb; pdb.set_trace()
+    # run
+    if options.trace:
+        import pdb; pdb.set_trace()
+
     with freedombuddy:
-        pass
+        webbrowser.open_new_tab(url)
 
     santiago.debug_log("Santiago finished!")
