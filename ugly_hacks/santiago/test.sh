470faed4bd6c4082c2405d7d0e6f399f40628151
Nick Daly <Nick.M.Daly@gmail.com>
2012-07-14 12:11:53 -0500

    Updated test.sh to reflect test moves.

--- ugly_hacks/santiago/test.sh
+++ ugly_hacks/santiago/test.sh
@@ -1,9 +1,10 @@
-#! /bin/sh # -*- mode: auto-fill; fill-column: 80 -*-
+#! /bin/sh # -*- mode: sh; mode: auto-fill; fill-column: 80 -*-
 
 PYTHONPATH=$PYTHONPATH:../..
 PYTHONPATH=$PYTHONPATH:/home/nick/programs/python-gnupg/python-gnupg-0.2.9
 export PYTHONPATH
 
-python test_pgpprocessor.py
-python test_santiago.py
+python tests/test_pgpprocessor.py
+python tests/test_santiago.py
+python tests/test_gnupg.py
 python protocols/https/test_controller.py
