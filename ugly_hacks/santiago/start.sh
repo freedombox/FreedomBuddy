7aa1f0aeab567ef5c7bac8c60385a643b6d12f4f
Nick Daly <Nick.M.Daly@gmail.com>
2012-08-13 22:10:58 -0500

    Update PYTHONPATH for rearranged source directory.

--- ugly_hacks/santiago/start.sh
+++ ugly_hacks/santiago/start.sh
@@ -1,8 +1,9 @@
 #! /bin/sh # -*- mode: auto-fill; fill-column: 80 -*-
 
-PYTHONPATH=$PYTHONPATH:../..
+cd src
+
+PYTHONPATH=$PYTHONPATH:../../..
 PYTHONPATH=$PYTHONPATH:/home/nick/programs/python-gnupg/python-gnupg-0.2.9
 export PYTHONPATH
 
-cd src
 python santiago_test.py "$@"
--- ugly_hacks/santiago/test.sh
+++ ugly_hacks/santiago/test.sh
@@ -1,10 +1,11 @@
 #! /bin/sh # -*- mode: sh; mode: auto-fill; fill-column: 80 -*-
 
-PYTHONPATH=$PYTHONPATH:../..
+cd src
+
+PYTHONPATH=$PYTHONPATH:../../..
 PYTHONPATH=$PYTHONPATH:/home/nick/programs/python-gnupg/python-gnupg-0.2.9
 export PYTHONPATH
 
-cd src
 python tests/test_pgpprocessor.py
 python tests/test_santiago.py
 python tests/test_gnupg.py
