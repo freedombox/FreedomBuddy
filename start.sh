06b8485de6ab410994289b246850d270b00b3c5f
Nick Daly <Nick.M.Daly@gmail.com>
2013-01-01 21:18:42 -0600

    Correct some paths.

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@
 cd src
 
 PYTHONPATH=$PYTHONPATH:../../..
+PYTHONPATH=$PYTHONPATH:../build/gnupg
 PYTHONPATH=$PYTHONPATH:../build/plinth
 PYTHONPATH=$PYTHONPATH:../build/python-gnupg-0.2.9
-PYTHONPATH=$PYTHONPATH:../build/bjsonrpc
+PYTHONPATH=$PYTHONPATH:../build/bjsonrpc/bjsonrpc
 export PYTHONPATH
 
 python santiago_test.py "$@"
