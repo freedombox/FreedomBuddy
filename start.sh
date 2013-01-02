708028f5f362cc1aa3bf98c15c53e93b1aceb8e3
Nick Daly <Nick.M.Daly@gmail.com>
2013-01-01 19:35:46 -0600

    Include bjsonrpc in the path.

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ cd src
 PYTHONPATH=$PYTHONPATH:../../..
 PYTHONPATH=$PYTHONPATH:/home/nick/programs/python-gnupg/python-gnupg-0.2.9
 PYTHONPATH=$PYTHONPATH:/home/nick/programs/freedombox/plinth
+PYTHONPATH=$PYTHONPATH:/home/nick/programs/freedombox/bjsonrpc
 export PYTHONPATH
 
 python santiago_test.py "$@"
