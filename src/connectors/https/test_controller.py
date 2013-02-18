292f4e73eeb7df1a83197c81f6b373e7b40a0c04
Nick Daly <Nick.M.Daly@gmail.com>
2013-02-18 09:49:53 -0600

    Completely rewrote https unit tests for 3-tier model.

--- src/connectors/https/test_controller.py
+++ src/connectors/https/test_controller.py
@@ -1,36 +1,44 @@
 """Tests for the HTTPS controller."""
 
 import cherrypy
-import ConfigParser as configparser
-import httplib, urllib
-import json
-import sys
-import time
-import unittest
-
 import connectors.https.controller as controller
-import santiago
+import optparse
+import sys
+import unittest
 import utilities
 
-import logging
-import gnupg
-
-
-logging.getLogger("cherrypy")
-logging.disable(9001) # it's over 9000!!!!
-
 class CherryPyTester(unittest.TestCase):
+    """Verify we're running the right CherryPy version.
 
+    If not, we'll silently get all kinds of errors, without obvious cause.
+
+    """
     def test_right_version(self):
         """CherryPy < 3.2 hoses things silently."""
 
         self.assertTrue([int(x) for x in cherrypy.__version__.split(".")]
                         >= [3,2])
 
-class RestTester(unittest.TestCase):
+class MonitorTest(unittest.TestCase):
+    """Make testing controllers easier."""
+
+    def command(self, aCommand):
+        """Record arguments."""
+
+        self.aCommand = aCommand
+
+    def assertInCommand(self, commands):
+        """Verify that all the commands are in the command line."""
+
+        return map(self.assertInTuple, [(x, self.aCommand) for x in commands])
+
+    def setUp(self):
+        """Replace the actual command execution with our override."""
+
+        controller.command = self.command
 
     if sys.version_info < (2, 7):
-        """Add a poor man's forward compatibility."""
+        # Add a poor man's forward compatibility.
 
         class ContainsError(AssertionError):
             pass
@@ -39,232 +47,202 @@ class RestTester(unittest.TestCase):
             if not a in b:
                 raise self.ContainsError("%s not in %s" % (a, b))
 
-class MonitorTest(RestTester):
-    """Generic test-class.
+        def assertInTuple(self, a_b):
+            a, b = a_b
+            return self.assertIn(a, b)
 
-    This is, unfortunately, bad effect testing.  I'm depending on way too much
-    code to get things set up and this should be broken out much more.  However,
-    it's testable and, more than anything, I need to get this under test.
+class Stopper(MonitorTest):
+    """Test the "HttpStop" controller."""
 
-    """
-    def setUp(self):
-        """Create my FBuddy, start it, and make a connector to it."""
+    def test_stop_stops(self):
+        """Stop must send the "--stop" command to the cli client."""
 
-        super(MonitorTest, self).setUp()
-
-        self.conn = httplib.HTTPSConnection("localhost", 8080)
-
-        # import pdb; pdb.set_trace()
-        self.santiago = self.create_santiago()
-        self.santiago.__enter__()
-
-    def create_santiago(self):
-	gpg_to_use = gnupg.GPG(gnupghome='../data/test_gpg_home')
-        # get my key, if possible
         try:
-            mykey = utilities.load_config("../data/test.cfg").get(
-                "pgpprocessor", "keyid")
-        except configparser.NoSectionError:
-            mykey = 0
+            controller.HttpStop().POST()
+        except cherrypy.HTTPRedirect:
+            pass
 
-        # set up monitors, listeners, and senders
-        cert = "../data/freedombuddy.crt"
-        connector = "https"
-        service = "freedombuddy"
-        location = "https://localhost:"
-        serving_port = 8080
+        self.assertEqual(self.aCommand, "--stop")
 
-        listeners = { connector: { "socket_port": serving_port,
-                                 "ssl_certificate": cert,
-                                 "ssl_private_key": cert
-                                  }, }
-        senders = { connector: { "proxy_host": "localhost",
-                               "proxy_port": 8118} }
-        monitors = { connector: {} }
+    def test_stop_redirects(self):
+        """Stop redirects to ``/freedombuddy`` after POSTing."""
 
-        # services to host and consume
-        hosting = { mykey: { service: [location + str(serving_port)] } }
-        consuming = { mykey: { service: [location + str(serving_port)] } }
-
-        # go!
-        return santiago.Santiago(listeners, senders,
-                                 hosting, consuming,
-                                 me=mykey, monitors=monitors,
-				 gpg = gpg_to_use)
-
-    def tearDown(self):
-        self.santiago.live = 0
-        self.santiago.__exit__(None, 0, None)
-
-    def get_args(self, *args, **kwargs):
-        """Record arguments."""
-
-        self.args = args
-        self.kwargs = kwargs
-
-class HttpMonitorTest(unittest.TestCase):
-    def setUp(self):
-        self.monitor = controller.HttpMonitor(None)
-
-class HttpMonitorQueryTest(HttpMonitorTest):
-    """Test HttpMonitor's Queries."""
-
-    def test_full_urls(self):
-        url = "https://localhost:8080/index?something=somethingelse"
-        self.assertEqual( { "something": "somethingelse" },
-                          self.monitor._parse_query(url) )
-
-    def test_query_strings(self):
-        url = "something=somethingelse"
-        self.assertEqual( { "something": "somethingelse" },
-                          self.monitor._parse_query(url) )
-
-    def test_with_question(self):
-        url = "?something=somethingelse"
-        self.assertEqual( { "something": "somethingelse" },
-                          self.monitor._parse_query(url) )
-
-    def test_multiple_url_queries(self):
-        url = "https://localhost:8080/index?something=somethingelse&this=that&1=2"
-        self.assertEqual( { "something": "somethingelse",
-                            "this": "that",
-                            "1": "2" },
-                          self.monitor._parse_query(url) )
-
-    def test_multiple_string_queries(self):
-        url = "?something=somethingelse&this=that&1=2"
-        self.assertEqual( { "something": "somethingelse",
-                            "this": "that",
-                            "1": "2" },
-                          self.monitor._parse_query(url) )
-
-class HttpMonitorRespondTest(HttpMonitorTest):
-    """Nothing I can think of testing here.
-
-    It's really just verifying Cheetah's Templates.  Worthwhile, perhaps?
-    Still, not a high priority.
-
-    """
-    pass
-
-class StopTest(MonitorTest):
-    def test_post(self):
-        controller.query(self.conn, url="/stop")
-
-        self.assertFalse(self.santiago.live)
-
-class Learn(MonitorTest):
-
-    def setUp(self):
-        super(Learn, self).setUp()
-
-        self.service = "atest"
-        self.value = 3
-
-        self.santiago.create_hosting_location(self.santiago.me, self.service,
-                                              [self.value])
-
-    # def test_post(self):
-    #     """Make sure arguments are actually passed to the Santiago as expected.
-
-    #     """
-    #     self.santiago.query = self.args
-
-    #     raise NotImplemented("ohnoes!")
-
-    def test_learn_hosted_services(self):
-        """Make sure requests to learners actually result in learned services.
-
-        """
-        controller.query(self.conn, "learn", self.santiago.me, self.service,
-                         action="POST")
-
-        self.assertEqual(
-            self.santiago.get_client_locations(self.santiago.me, self.service),
-            self.value)
-
-    def test_redirected_to_service(self):
-        """The HTTPS controller redirects to the learned service's location."""
-
-        data = controller.query(self.conn, "learn", self.santiago.me,
-                                self.service, action="POST")
-
-        self.assertIn("""\
-This resource can be found at <a href='https://{0}:{1}/consuming/{2}/{3}'>\
-""".format(self.conn.host, self.conn.port, self.santiago.me, self.service),
-                      data)
+        self.assertRaises(cherrypy.HTTPRedirect,
+                          controller.HttpStop().POST)
 
 class Listener(MonitorTest):
-    """External incoming-request listener.
+    """Test the "HttpListener" controller."""
 
-    All HTTPS requests and replies eventually funnel into here, so if this won't
-    work, nothing will.
+    def read_request(self, *args, **kwargs):
+        return "request=aRequest"
 
-    """
+    def test_listen_listens(self):
+        """Listeners must send "--request" to the cli client."""
+
+        fakefile = lambda: None
+        fakefile.read = self.read_request
+
+        cherrypy.request.body = fakefile
+        controller.HttpsListener().index()
+        cherrypy.request.body = None
+        self.assertInCommand(["--request aRequest"])
+
+class Hosting(MonitorTest):
+    """Test the "HttpHosting" controller."""
+
     def setUp(self):
-        """Create a FreedomBuddy that knows about itself."""
+        super(Hosting, self).setUp()
+        self.controller = controller.HttpHosting()
+        self.controller.respond = lambda x,y: None
 
-        super(Listener, self).setUp()
+    def test_get(self):
+        self.controller.GET()
+        self.assertInCommand(("--action list", "--hosting"))
 
-        self.service = "atest"
-        self.value = 3
+    def test_put(self):
+        self.controller.PUT("a")
+        self.assertInCommand(("--action add", "--hosting", "--key a"))
 
-        self.santiago.create_hosting_location(self.santiago.me, self.service,
-                                              [self.value])
+    def test_delete(self):
+        self.controller.DELETE("a")
+        self.assertInCommand(("--action remove", "--hosting", "--key a"))
 
-        self.request = {
-            "request":
-                self.santiago.gpg.encrypt(
-                json.dumps({ "host": self.santiago.me,
-                             "client": self.santiago.me,
-                             "service": self.service,
-                             "locations": [self.value],
-                             "reply_to": [],
-                             "request_version": 1,
-                             "reply_versions":
-                                 list(santiago.Santiago.SUPPORTED_CONNECTORS),}),
+class HostedClient(MonitorTest):
+    """Test the "HttpHostedClient" controller."""
 
-            self.santiago.me,
-            sign=self.santiago.me)}
+    def setUp(self):
+        super(HostedClient, self).setUp()
+        self.controller = controller.HttpHostedClient()
+        self.controller.respond = lambda x,y: None
 
-    def test_learn_hosted_services(self):
-        """Make sure requests to listeners actually result in learned services.
+    def test_get(self):
+        self.controller.GET("a")
+        self.assertInCommand(("--action list", "--hosting", "--key a"))
 
-        FIXME: or rather, fix controller::Listener::index.  I shouldn't be able
-        to GET here.
+    def test_put(self):
+        self.controller.PUT("a", "b")
+        self.assertInCommand(
+            ("--action add", "--hosting", "--key a", "--service b"))
 
-        """
-        controller.query(self.conn, "/", params=self.request)
+    def test_delete(self):
+        self.controller.DELETE("a", "b")
+        self.assertInCommand(
+            ("--action remove", "--hosting", "--key a", "--service b"))
 
-        time.sleep(1)
+class HostedService(MonitorTest):
+    """Test the "HttpHostedService" controller."""
 
-        self.assertEqual(
-            self.santiago.get_client_locations(self.santiago.me, self.service),
-            self.value)
+    def setUp(self):
+        super(HostedService, self).setUp()
+        self.controller = controller.HttpHostedService()
+        self.controller.respond = lambda x,y: None
 
-    def test_learn_hosted_services_the_right_way(self):
-        """Make sure requests to listeners actually result in learned services.
+    def test_get(self):
+        self.controller.GET("a", "b")
+        self.assertInCommand(
+            ("--action list", "--hosting", "--key a", "--service b"))
 
-        """
-        controller.query(self.conn, "/", action="POST", body=self.request)
+    def test_put(self):
+        self.controller.PUT("a", "b", "c")
+        self.assertInCommand(
+            ("--action add", "--hosting", "--key a", "--service b",
+             "--location c"))
 
-        time.sleep(1)
+    def test_delete(self):
+        self.controller.DELETE("a", "b", "c")
+        self.assertInCommand(
+            ("--action remove", "--hosting", "--key a", "--service b",
+             "--location c"))
 
-        self.assertEqual(
-            self.santiago.get_client_locations(self.santiago.me, self.service),
-            self.value)
+class Consuming(MonitorTest):
+    """Test the "HttpConsuming" controller."""
 
-    def test_catch_outgoing_request(self):
-        """If I make an outgoing request, does the listener hear it?"""
+    def setUp(self):
+        super(Consuming, self).setUp()
+        self.controller = controller.HttpConsuming()
+        self.controller.respond = lambda x,y: None
 
-        self.santiago.listeners["https"].incoming_request = self.get_args
+    def test_get(self):
+        self.controller.GET()
+        self.assertInCommand(("--action list", "--consuming"))
 
-        id = self.santiago.me
-        self.santiago.outgoing_request(id, id, id, id, self.service)
+    def test_put(self):
+        self.controller.PUT("a")
+        self.assertInCommand(("--action add", "--consuming", "--key a"))
 
-        self.assertTrue(self.kwargs["request"])
+    def test_delete(self):
+        self.controller.DELETE("a")
+        self.assertInCommand(("--action remove", "--consuming", "--key a"))
 
+class ConsumedHost(MonitorTest):
+    """Test the "HttpConsumedHost" controller."""
 
+    def setUp(self):
+        super(ConsumedHost, self).setUp()
+        self.controller = controller.HttpConsumedHost()
+        self.controller.respond = lambda x,y: None
+
+    def test_get(self):
+        self.controller.GET("a")
+        self.assertInCommand(("--action list", "--consuming", "--key a"))
+
+    def test_put(self):
+        self.controller.PUT("a", "b")
+        self.assertInCommand(
+            ("--action add", "--consuming", "--key a", "--service b"))
+
+    def test_delete(self):
+        self.controller.DELETE("a", "b")
+        self.assertInCommand(
+            ("--action remove", "--consuming", "--key a", "--service b"))
+
+class ConsumedService(MonitorTest):
+    """Test the "HttpConsumedService" controller."""
+
+    def setUp(self):
+        super(ConsumedService, self).setUp()
+        self.controller = controller.HttpConsumedService()
+        self.controller.respond = lambda x,y: None
+
+    def test_get(self):
+        self.controller.GET("a", "b")
+        self.assertInCommand(
+            ("--action list", "--consuming", "--key a", "--service b"))
+
+    def test_put(self):
+        self.controller.PUT("a", "b", "c")
+        self.assertInCommand(
+            ("--action add", "--consuming", "--key a", "--service b",
+             "--location c"))
+
+    def test_delete(self):
+        self.controller.DELETE("a", "b", "c")
+        self.assertInCommand(
+            ("--action remove", "--consuming", "--key a", "--service b",
+             "--location c"))
+
+class Query(MonitorTest):
+    """Test the "HttpQuery" controller."""
+
+    def setUp(self):
+        super(Query, self).setUp()
+        self.controller = controller.HttpQuery()
+
+    def test_post(self):
+        """Do requests hook into the CLI client?"""
+
+        try:
+            self.controller.POST("a", "b")
+        except cherrypy.HTTPRedirect:
+            pass
+        self.assertInCommand(
+            ("--query", "--key a", "--service b"))
+
+    def test_post_redirects(self):
+        """Are requests redirected appropriately?"""
+
+        self.assertRaises(cherrypy.HTTPRedirect,
+                          self.controller.POST, *("a", "b"))
 
 if __name__ == "__main__":
     unittest.main()
